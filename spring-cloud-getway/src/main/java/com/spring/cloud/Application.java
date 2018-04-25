0a6c77ca2ced1ebbe4cd43afb56665860e197795
duanhong <2114517411@qq.com>
2018-04-25 22:54:26 +0800

    tijiao

--- spring-cloud-getway/src/main/java/com/spring/cloud/Application.java
+++ spring-cloud-getway/src/main/java/com/spring/cloud/Application.java
@@ -2,10 +2,10 @@ package com.spring.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringCloudApplication
-@EnableDiscoveryClient
+@EnableZuulProxy
 public class Application {
 	public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
--- spring-cloud-govern/src/main/java/com/spring/cloud/Application.java
+++ spring-cloud-govern/src/main/java/com/spring/cloud/Application.java
@@ -2,10 +2,10 @@ package com.spring.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
-import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringCloudApplication
-@
+@EnableDiscoveryClient
 public class Application {
 	public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
