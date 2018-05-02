6582eadb3eb8eb802158d10347ac4fbac5b541b7
duanhong <2114517411@qq.com>
2018-05-03 01:14:23 +0800

    提交

---
+++ spring-cloud-server-one/src/main/java/com/spring/cloud/controller/IndexController.java
@@ -0,0 +1,20 @@
+package com.spring.cloud.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class IndexController {
+	
+	private Logger log=LoggerFactory.getLogger(this.getClass());
+	@ResponseBody
+	@RequestMapping("index")
+	public String index() {
+		log.info("Server1.index");
+		return "Hello World one";
+	}
+
+}
